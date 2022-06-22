391f17e6041532c49d23b11561a8a59ef0dce8de
eldesernauta <oscarrojascs@gmail.com>
2022-06-22 08:02:59 -0500

    first commit with React structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './app.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='content'>
+        <h1 className='name-title'>Oscar Rojas</h1>
+        <h2>Web Dev </h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
