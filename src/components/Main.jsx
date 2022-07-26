439c45ee805a484477adb707b3d80913029e0184
eldesernauta <oscarrojascs@gmail.com>
2022-07-25 19:47:05 -0500

    Absolute Structure

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,13 +5,14 @@ import FrthBox from "./FrthBox";
 import "../styles/main.scss";
 
 const Main = () => {
+
   return (
     <>
       <div className="row">
-        <FirstBox className="Box1"/>
-        <ScndBox className="Box2"/>
-        <ThirdBox className="Box3"/>
-        <FrthBox className="Box4"/>
+        <FirstBox className="Box1" />
+        <ScndBox className="Box2" />
+        <ThirdBox className="Box3" />
+        <FrthBox className="Box4" />
       </div>
     </>
   );
