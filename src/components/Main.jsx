43f653385e73ca022297d78907b6090170196012
eldesernauta <oscarrojascs@gmail.com>
2022-07-30 22:50:08 -0500

    Education/Skills section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,10 +2,16 @@ import { useState } from "react";
 import "../styles/main.scss";
 import pic from "../assets/portrait.png";
 
+import ProgressBar from "@ramonak/react-progress-bar";
+
 const Main = () => {
   const [isFold1, setFold1] = useState("false");
   const [isFold2, setFold2] = useState("false");
   const [isFold3, setFold3] = useState("false");
+
+  const Box1 = document.querySelector(".Box1");
+  const Box2 = document.querySelector(".Box2");
+  const Box3 = document.querySelector(".Box3");
 
   const handleToggle1 = () => {
     setTimeout(() => {
@@ -15,17 +21,17 @@ const Main = () => {
     const BoxActive = document.querySelector(".Box1");
 
     if (!BoxActive.classList.contains("Box1-fold")) {
-      document.querySelector(".Box1").style.minHeight = "90vh";
-      document.querySelector(".Box1").style.maxHeight = "90vh";
-      document.querySelector(".Box3").style.maxHeight = "10vh";
-      document.querySelector(".Box3").style.minHeight = "10vh";
+      Box1.style.minHeight = "90vh";
+      Box1.style.maxHeight = "90vh";
+      Box3.style.maxHeight = "10vh";
+      Box3.style.minHeight = "10vh";
       document.getElementById("btnBox2").style.display = "none";
       document.getElementById("btnBox3").style.display = "none";
     } else {
-      document.querySelector(".Box1").style.minHeight = "50vh";
-      document.querySelector(".Box1").style.maxHeight = "50vh";
-      document.querySelector(".Box3").style.maxHeight = "50vh";
-      document.querySelector(".Box3").style.minHeight = "50vh";
+      Box1.style.minHeight = "50vh";
+      Box1.style.maxHeight = "50vh";
+      Box3.style.maxHeight = "50vh";
+      Box3.style.minHeight = "50vh";
       document.getElementById("btnBox2").style.display = "block";
       document.getElementById("btnBox3").style.display = "block";
     }
@@ -39,17 +45,21 @@ const Main = () => {
     const BoxActive = document.querySelector(".Box2");
 
     if (!BoxActive.classList.contains("Box2-fold")) {
-      document.querySelector(".Box2").style.minWidth = "90vw";
-      document.querySelector(".Box1").style.maxWidth = "10vw";
+      Box2.style.minWidth = "90vw";
+      Box2.style.maxWidth = "90vw";
+      Box1.style.maxWidth = "10vw";
+      Box1.style.minWidth = "10vw";
       document.getElementById("btnBox1").style.display = "none";
       document.getElementById("btnBox3").style.display = "none";
-      document.querySelector(".Box1").querySelector("h1").style.fontSize = "40px";
+      Box1.querySelector("h1").style.fontSize = "40px";
     } else {
-      document.querySelector(".Box2").style.minWidth = "50vw";
-      document.querySelector(".Box1").style.maxWidth = "50vw";
+      Box2.style.minWidth = "50vw";
+      Box2.style.maxWidth = "50vw";
+      Box1.style.maxWidth = "50vw";
+      Box1.style.minWidth = "50vw";
       document.getElementById("btnBox1").style.display = "block";
       document.getElementById("btnBox3").style.display = "block";
-      document.querySelector(".Box1").querySelector("h1").style.fontSize = "58px";
+      Box1.querySelector("h1").style.fontSize = "58px";
     }
   };
 
@@ -61,23 +71,21 @@ const Main = () => {
     const BoxActive = document.querySelector(".Box3");
 
     if (!BoxActive.classList.contains("Box3-fold")) {
-      document.querySelector(".Box3").style.minHeight = "90vh";
-      document.querySelector(".Box3").style.maxHeight = "90vh";
-      document.querySelector(".Box1").style.maxHeight = "10vh";
-      document.querySelector(".Box1").style.minHeight = "10vh";
+      Box3.style.minHeight = "90vh";
+      Box3.style.maxHeight = "90vh";
+      Box1.style.maxHeight = "10vh";
+      Box1.style.minHeight = "10vh";
       document.getElementById("btnBox1").style.display = "none";
       document.getElementById("btnBox2").style.display = "none";
-      document.querySelector(".Box1").querySelector(".content").style.padding = "0";
-
+      Box1.querySelector(".content").style.padding = "0";
     } else {
-      document.querySelector(".Box3").style.minHeight = "50vh";
-      document.querySelector(".Box3").style.maxHeight = "50vh";
-      document.querySelector(".Box1").style.maxHeight = "50vh";
-      document.querySelector(".Box1").style.minHeight = "50vh";
+      Box3.style.minHeight = "50vh";
+      Box3.style.maxHeight = "50vh";
+      Box1.style.maxHeight = "50vh";
+      Box1.style.minHeight = "50vh";
       document.getElementById("btnBox1").style.display = "block";
       document.getElementById("btnBox2").style.display = "block";
-      document.querySelector(".Box1").querySelector(".content").style.padding = "2em";
-
+      Box1.querySelector(".content").style.padding = "2em";
     }
   };
   return (
@@ -86,7 +94,7 @@ const Main = () => {
         <div className={isFold1 ? "Box1" : "Box1 Box1-fold"}>
           <div className="content">
             <h1>Oscar Rojas</h1>
-            <h4>Web Developer</h4>
+            <p className="subtitle">Web Developer / Photographer</p>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -119,8 +127,138 @@ const Main = () => {
 
         <div className={isFold2 ? "Box2" : "Box2 Box2-fold"}>
           <div className="content">
-            <h2>Estudios</h2>
-
+            <h2>Education / Skills</h2>
+            <div className="grid-studies">
+              <div className="grid-education">
+                <div className="grid-ed">
+                  <h3>PUBLICIST</h3>
+                  <p>
+                    Degree in Advertising
+                    <br />
+                    (Armenia, Quindío)
+                  </p>
+                  <h6>
+                    E.A.M
+                    <br />
+                    2012 - 2017
+                  </h6>
+                </div>
+                <div className="grid-ed">
+                  <h3>MISIONTIC 2022</h3>
+                  <p>
+                    Member at the 2nd Route of the fullstack web development
+                    training imparted by the colombian ICT ministry
+                    <br />
+                    (Medellín, Antioquia)
+                  </p>
+                  <h6>
+                    University of Antioquia
+                    <br />
+                    Mar. 2021 - Dic. 2021
+                  </h6>
+                </div>
+                <div className="grid-ed">
+                  <h3>FULL STACK WEB DEVELOPMENT MASTER</h3>
+                  <p>
+                    Active student in Master’s degree focused on Full Stack web
+                    development.
+                    <br />
+                    (Catalonia, Spain)
+                  </p>
+                  <h6>
+                    Universitat Politècnica de Catalunya
+                    <br />
+                    May. 2022 - May. 2023
+                  </h6>
+                </div>
+              </div>
+              <div className="grid-skills">
+                <div className="grid-skill">
+                  <ProgressBar
+                    completed={95}
+                    customLabel="HTML + CSS (Sass | Bootstrap | Tailwind)"
+                    className="progressBar"
+                    bgColor="#6a39ff"
+                    height="12px"
+                    labelSize="12px"
+                    labelAlignment="left"
+                    labelColor="#111"
+                    baseBgColor="#dbdbdb"
+                    borderRadius="0px"
+                  />
+                </div>
+                <div className="grid-skill">
+                  <ProgressBar
+                    completed={80}
+                    customLabel="Javascript (ReactJS | NodeJS | Webpack)"
+                    className="progressBar"
+                    bgColor="#6a39ff"
+                    height="12px"
+                    labelSize="12px"
+                    labelAlignment="left"
+                    labelColor="#111"
+                    baseBgColor="#dbdbdb"
+                    borderRadius="0px"
+                  />
+                </div>
+                <div className="grid-skill">
+                  <ProgressBar
+                    completed={50}
+                    customLabel="AWS"
+                    className="progressBar"
+                    bgColor="#6a39ff"
+                    height="12px"
+                    labelSize="12px"
+                    labelAlignment="left"
+                    labelColor="#111"
+                    baseBgColor="#dbdbdb"
+                    borderRadius="0px"
+                  />
+                </div>
+                <div className="grid-skill">
+                  <ProgressBar
+                    completed={70}
+                    customLabel="Git"
+                    className="progressBar"
+                    bgColor="#6a39ff"
+                    height="12px"
+                    labelSize="12px"
+                    labelAlignment="left"
+                    labelColor="#111"
+                    baseBgColor="#dbdbdb"
+                    borderRadius="0px"
+                  />
+                </div>
+                <div className="grid-skill">
+                  <ProgressBar
+                    completed={90}
+                    customLabel="Photoshop | Illustrator"
+                    className="progressBar"
+                    bgColor="#6a39ff"
+                    height="12px"
+                    labelSize="12px"
+                    labelAlignment="left"
+                    labelColor="#111"
+                    baseBgColor="#dbdbdb"
+                    borderRadius="0px"
+                  />
+                </div>
+                <div className="grid-skill">
+                  <ProgressBar
+                    completed={80}
+                    customLabel="English"
+                    className="progressBar"
+                    bgColor="#6a39ff"
+                    height="12px"
+                    labelSize="12px"
+                    labelAlignment="left"
+                    labelColor="#111"
+                    baseBgColor="#dbdbdb"
+                    borderRadius="0px"
+                  />
+                </div>
+              </div>
+            </div>
 
             <button
               type="button"
